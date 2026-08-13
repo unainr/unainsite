42c1162d30264871fc53afd55b1ef0327d621739
UNAINR <munain605@gmail.com>
2026-08-13 11:56:29 +0500

    change logo

--- components/layouts/logo.tsx
+++ components/layouts/logo.tsx
@@ -2,18 +2,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
-  return (
-    <Link href="/" className="flex items-center gap-2 invert-25 dark:invert-0">
-      <Image
-       className="h-auto w-auto"
-        src="/fine.png" // or your public path
-        alt="UNAIN Logo"
-        width={120}
-        height={50}
-       priority
-      />
-    </Link>
-  );
+	return (
+		<Link href="/" className="flex items-center gap-2 ">
+			<Image
+				className="h-auto w-auto hidden dark:block"
+				src="https://res.cloudinary.com/ttvcunbx/image/upload/c_crop,g_north_west,h_305,w_1101,x_74,y_468/logo22_qb6iob.png" // or your public path
+				alt="UNAIN Logo"
+				width={120}
+				height={50}
+				priority
+			/>
+			<Image
+				src="https://res.cloudinary.com/ttvcunbx/image/upload/e_background_removal/c_crop,g_north_west,h_308,w_1153,x_33,y_465/unain_emf2qo.png"
+						alt="UNAIN Logo"
+
+				width={120}
+				height={50}
+				priority
+				className="h-auto w-auto object-contain dark:hidden block"
+			/>
+		</Link>
+	);
 };
 
 export default Logo;
